598b598040b274d9e2abd5b3478de485944f4129
Antonio Muñoz Sánchez <stormman666@Antonios-MacBook-Pro.local>
2017-11-14 11:50:01 +0100

    newideamaster

--- FunctBranches/ViewController.swift
+++ FunctBranches/ViewController.swift
@@ -31,6 +31,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        //new idea master 01
     }
 
 
