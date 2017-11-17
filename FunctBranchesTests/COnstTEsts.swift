b46bd1db01966f9b91787dd75bc4eef836a54356
Antonio Muñoz Sánchez <stormman666@Antonios-MacBook-Pro.local>
2017-11-17 07:28:36 +0100

    add const test

---
+++ FunctBranchesTests/COnstTEsts.swift
@@ -0,0 +1,40 @@
+//
+//  COnstTEsts.swift
+//  FunctBranchesTests
+//
+//  Created by Antonio Muñoz Sánchez on 16/11/2017.
+//  Copyright © 2017 Antonio Muñoz Sánchez. All rights reserved.
+//
+
+import XCTest
+
+@testable import FunctBranches
+
+class COnstTEsts: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        
+        let cf = Const<String,Int> ({return "pe"})
+        
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
